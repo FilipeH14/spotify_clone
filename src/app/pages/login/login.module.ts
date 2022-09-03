37c789b863fc3aa1e3fbc5dcb442c3edb24970e8
Filipe Haniel Martins <filipe.martins@qwti.net.br>
2022-09-02 23:19:24 -0300

    inicio tela de login

---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
+
+import { LoginComponent } from './login.component'
+import { loginRotas } from './login.routes'
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(loginRotas)
+  ]
+})
+export class LoginModule { }
