fb7783643a7b2c0615fb4ba4e91724c326242270
filipe haniel <you@example.com>
2022-11-10 09:13:12 -0300

    correção

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -18,7 +18,7 @@ export class SpotifyService {
     const authEndpoint = `${SpotifyConfiguration.authEndpoint}?`
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`
     const redirectUrl = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`
-    const scopes = `scope${SpotifyConfiguration.scopes.join('%20')}&`
+    const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}&`
     const responseType = `response_type=token&show_dialog=true`
 
     return authEndpoint + clientId + redirectUrl + scopes + responseType
